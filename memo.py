ab310c0fba3dcf14ac6052202a71d07869b76ffe
wooyoung-4723 <wy4723@naver.com>
2026-02-09 16:36:51 +0900

    tkinter로 간단한 창 구성

---
+++ memo.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+window = Tk()
+window.title("Memo")
+window.geometry("500x500")
+window.resizable(width=True, height=True)
+
+menu = Menu(window)
+menu_1 = Menu(menu, tearoff=0)
+menu_1.add_command(label = "새파일")
+menu_1.add_command(label='저장')
+menu_1.add_separator()
+menu_1.add_command(label ="종료", command = window.destroy)
+menu.add_cascade(label = "파일", menu = menu_1)
+
+
+menu_2 = Menu(menu, tearoff=0)
+menu_2.add_command(label="만든이")
+menu.add_cascade(label= "만든이", menu = menu_2)
+
+window.config(menu = menu)
+window.mainloop()
